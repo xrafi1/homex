93613ec76e88eadff0c2254ddd698d8a92196dd2
xrafi1 <0xyzrr@gmail.com>
2025-07-04 17:12:26 +0530

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'config.php';
+
+class Database {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function query($sql) {
+        return $this->conn->query($sql);
+    }
+
+    public function escape($value) {
+        return $this->conn->real_escape_string($value);
+    }
+
+    public function insertId() {
+        return $this->conn->insert_id;
+    }
+
+    public function __destruct() {
+        $this->conn->close();
+    }
+}
+?>
